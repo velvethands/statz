cbd402b90572fcf0e5c2e2c953a1ca9a6209a711
Rasmus Back <rasmus.back@reaktor.fi>
2013-03-30 17:59:17 +0200

    Refactor

--- js/app-require.js
+++ js/app-require.js
@@ -1,11 +1,19 @@
 require.config({
   baseUrl: 'js',
   paths: {
-    'jquery': '../components/jquery/jquery',
-    'bacon': '../components/bacon/dist/Bacon',
-    'handlebars': '../components/handlebars/handlebars',
-    'lessjs': '../components/less.js/dist/less-1.3.3',
-    'text': '../components/requirejs-text/text'
+    $: '../components/jquery/jquery',
+    bacon: '../components/bacon/dist/Bacon',
+    handlebars: '../components/handlebars/handlebars',
+    lessjs: '../components/less.js/dist/less-1.3.3',
+    text: '../components/requirejs-text/text'
+  },
+  shim: {
+    '$': {
+      exports: '$'
+    },
+    'Handlebars': {
+      exports: 'Handlebars'
+    }
   }
 });
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-define(['text!../html/templates.html', 'jquery', 'bacon', 'handlebars', 'lessjs'], function(html) {
+define(['text!../html/templates.html', '$', 'bacon', 'handlebars', 'lessjs'], function(html) {
   return function() {
     var template = Handlebars.compile(html)
     return template()

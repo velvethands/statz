37650bfa90a44da1e27260e9e4ff6236b099e02e
Rasmus Back <rasmus.back@reaktor.fi>
2013-07-10 22:55:51 +0300

    Add mongo support

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
-ruby '1.9.2'
+ruby '1.9.3'
 source 'https://rubygems.org'
 gem 'sinatra', '1.4.2'
 gem 'json', '1.8.0'
+gem 'mongo', '1.9.1'
+gem 'bson_ext', '1.9.1'
 
 group :development do
   gem 'foreman', '0.63.0'
--- rest.rb
+++ rest.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 require 'json'
+require 'mongo'
+include Mongo
+
+mongo_uri = ENV['MONGOLAB_URI']
+db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
+client = MongoClient.from_uri(mongo_uri)
+db = client.db(db_name)
+db.collection_names.each { |name| puts name }
 
 get '/rest/health-check' do
   status 200
